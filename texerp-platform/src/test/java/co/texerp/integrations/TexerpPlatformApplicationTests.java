a54878aa5d84f2707adf72390c8b1c8fc8b7caf2
MariaFernanda1818 <mariafernandavalencian27@gmail.com>
2026-09-08 11:40:41 -0500

    Add database connection configuration to the VPS.

--- texerp-platform/src/test/java/co/texerp/integrations/TexerpPlatformApplicationTests.java
+++ texerp-platform/src/test/java/co/texerp/integrations/TexerpPlatformApplicationTests.java
@@ -2,8 +2,10 @@ package co.texerp.integrations;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TexerpPlatformApplicationTests {
 
 	@Test
